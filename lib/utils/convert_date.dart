b18cbd5a3bdf3fcbba41c5bdfbdf8877fb92ce23
sidverma <siddharthvermas123456@gmail.com>
2020-07-23 00:26:03 +0530

    add

--- lib/utils/convert_date.dart
+++ lib/utils/convert_date.dart
@@ -3,7 +3,7 @@ import 'package:intl/intl.dart';
 class ConvertDate {
 
   String convertToMMMMMddyyyy(String dateTimeString)  {
-    DateTime dateTime=DateFormat('yyyy-mm-dd').parse(dateTimeString);
+    DateTime dateTime=DateFormat('yyyy-MM-dd').parse(dateTimeString);
     dateTimeString=DateFormat('MMMM dd, yyyy').format(dateTime);
     return dateTimeString;
   }
